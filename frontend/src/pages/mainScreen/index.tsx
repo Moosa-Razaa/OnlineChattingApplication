a89234d180453085a6fcd74cfc1140f1a074f865
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-01-04 23:00:46 +0100

    User list and header is almost completed

--- frontend/src/pages/mainScreen/index.tsx
+++ frontend/src/pages/mainScreen/index.tsx
@@ -1,18 +1,31 @@
 import { Layout } from "antd";
 import style from "./styles.module.css";
 import UsersList from "./UsersList";
+import UserInfoHeader from "./userInfoHeader";
 
 const { Header, Sider, Content } = Layout;
+
+const userInfoHeaderProps: UserInfoHeaderProps = {
+  UserName: "You",
+  ProfilePicture: "https://api.dicebear.com/7.x/miniavs/svg?seed=$0",
+  Status: new Date(),
+};
 
 function MainScreen() {
   return (
     <div className={style["mainDiv"]}>
-        <Layout className={style["mainLayout"]}>
+      <Layout className={style["mainLayout"]}>
         <Sider width="25%" className={style["sider"]}>
           <UsersList />
         </Sider>
         <Layout className={style["mainContentLayout"]}>
-          <Header className={style["header"]}>Header</Header>
+          <Header className={style["header"]}>
+            <UserInfoHeader
+              UserName={userInfoHeaderProps.UserName}
+              ProfilePicture={userInfoHeaderProps.ProfilePicture}
+              Status={userInfoHeaderProps.Status}
+            />
+          </Header>
           <Content className={style["content"]}>Content</Content>
         </Layout>
       </Layout>
