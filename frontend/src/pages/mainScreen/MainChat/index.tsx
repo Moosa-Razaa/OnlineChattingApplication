53d9d86ffedb89e672f925722ca4907d076369ed
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-01-09 17:08:28 +0100

    Design the chatting design

--- frontend/src/pages/mainScreen/MainChat/index.tsx
+++ frontend/src/pages/mainScreen/MainChat/index.tsx
@@ -1,13 +1,27 @@
-import { Button, Input } from "antd";
+import { Button, Input, Typography } from "antd";
 import emoji from "../../../assets/Emoji.svg";
 import upload from "../../../assets/Upload.svg";
 import audioRecord from "../../../assets/AudioRecord.svg";
 import style from "./styles.module.css";
 
+const { Paragraph } = Typography;
+
 export default function MainChat() {
   return (
     <div className={style["mainDiv"]}>
-      <div className={style["chatDiv"]}></div>
+      <div className={style["chatDiv"]}>
+        <div className={style["leftAlignedDiv"]}>
+          <Paragraph className={style["message"]}>
+            Hello! How are you?
+          </Paragraph>
+        </div>
+        <div className={style["rightAlignedDiv"]}>
+          <Paragraph>
+            I am fine. What about you?
+          </Paragraph>
+        </div>
+      </div>
+
       <div className={style["textingDiv"]}>
         <Button type="link" className={style["insertEmojiButton"]}>
           <img className={style["insertEmojiImage"]} src={emoji} />
