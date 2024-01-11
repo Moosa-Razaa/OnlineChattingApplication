829e727692eef0b0b0a01e50cf9aca67d4e67bb8
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-01-11 21:43:33 +0100

    Added individual and group chat component

--- frontend/src/pages/mainScreen/MainChat/IndividualChat/index.tsx
+++ frontend/src/pages/mainScreen/MainChat/IndividualChat/index.tsx
@@ -1,7 +1,4 @@
-import { Button, Input, Typography } from "antd";
-import emoji from "../../../../assets/Emoji.svg";
-import upload from "../../../../assets/Upload.svg";
-import audioRecord from "../../../../assets/AudioRecord.svg";
+import { Typography } from "antd";
 import style from "./styles.module.css";
 import { IndividualMessage } from "./types";
 import { format } from "date-fns";
@@ -25,60 +22,38 @@ const chat: IndividualMessage[] = [
 
 function IndividualChat() {
   return (
-    <div className={style["mainDiv"]}>
-      <div className={style["chatDiv"]}>
-        {chat.map((currentItem) => {
-          return (
-            <div
-              id={`${currentItem.MessageId}`}
-              className={`${style["messageDiv"]}
+    <>
+      {chat.map((currentItem) => {
+        return (
+          <div
+            key={`${currentItem.MessageId}`}
+            className={`${style["messageDiv"]}
                 ${
                   currentItem.IsReceived
                     ? style["leftAlignedDiv"]
                     : style["rightAlignedDiv"]
                 }`}
-            >
-              <div className={style["inidividualMessage"]}>
-                <Paragraph
-                  className={`${
-                    currentItem.IsReceived ? style["sentMessage"] : ""
-                  }`}
-                >
-                  {currentItem.Message}
-                </Paragraph>
-                <Text
-                  className={`${style["messageTimeText"]} ${
-                    currentItem.IsReceived ? style["sentMessage"] : ""
-                  }`}
-                >
-                  {format(currentItem.SendAt, "HH:mm")}
-                </Text>
-              </div>
+          >
+            <div className={style["inidividualMessage"]}>
+              <Paragraph
+                className={`${
+                  currentItem.IsReceived ? style["sentMessage"] : ""
+                }`}
+              >
+                {currentItem.Message}
+              </Paragraph>
+              <Text
+                className={`${style["messageTimeText"]} ${
+                  currentItem.IsReceived ? style["sentMessage"] : ""
+                }`}
+              >
+                {format(currentItem.SendAt, "HH:mm")}
+              </Text>
             </div>
-          );
-        })}
-      </div>
-
-      <div className={style["textingDiv"]}>
-        <Button type="link" className={style["insertEmojiButton"]}>
-          <img className={style["insertEmojiImage"]} src={emoji} />
-        </Button>
-        <Input
-          className={style["messageInput"]}
-          count={{
-            show: true,
-            max: 500,
-          }}
-          placeholder="Your message..."
-        />
-        <Button type="link" className={style["insertEmojiButton"]}>
-          <img className={style["insertEmojiImage"]} src={upload} />
-        </Button>
-        <Button type="link" className={style["insertEmojiButton"]}>
-          <img className={style["insertEmojiImage"]} src={audioRecord} />
-        </Button>
-      </div>
-    </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
 
--- frontend/src/pages/mainScreen/MainChat/index.tsx
+++ frontend/src/pages/mainScreen/MainChat/index.tsx
@@ -1,7 +1,40 @@
+import { Button, Input } from "antd";
+import GroupChat from "./GroupChat";
 import IndividualChat from "./IndividualChat";
+import style from "./styles.module.css";
+
+import emoji from "../../../assets/Emoji.svg";
+import upload from "../../../assets/Upload.svg";
+import audioRecord from "../../../assets/AudioRecord.svg";
+
+const isGroupChat = true;
 
 export default function MainChat() {
   return (
-    <IndividualChat />
+    <div className={style["mainDiv"]}>
+      <div className={style["chatDiv"]}>
+        {isGroupChat ? <GroupChat /> : <IndividualChat />}
+      </div>
+
+      <div className={style["textingDiv"]}>
+        <Button type="link" className={style["insertEmojiButton"]}>
+          <img className={style["insertEmojiImage"]} src={emoji} />
+        </Button>
+        <Input
+          className={style["messageInput"]}
+          count={{
+            show: true,
+            max: 500,
+          }}
+          placeholder="Your message..."
+        />
+        <Button type="link" className={style["insertEmojiButton"]}>
+          <img className={style["insertEmojiImage"]} src={upload} />
+        </Button>
+        <Button type="link" className={style["insertEmojiButton"]}>
+          <img className={style["insertEmojiImage"]} src={audioRecord} />
+        </Button>
+      </div>
+    </div>
   );
 }
