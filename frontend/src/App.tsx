5bcd8b701e59352dd85896a2a3ac539491cd44d3
Moosa-Razaa <moosaraza1k99@gmail.com>
2024-01-15 14:17:46 +0100

    Adding React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
+import { RouterProvider } from "react-router-dom";
 import style from "./App.module.css";
-import MainScreen from "./pages/mainScreen";
+import routes from "./services/routes";
 
 function App() {
   
   return (
       <div className={style["mainDiv"]}>
-        <MainScreen />
+        <RouterProvider router={routes} />
       </div>
   )
 }
