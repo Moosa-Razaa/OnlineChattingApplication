593381e0bc2a02a19c2cc047468108ffdc318a4b
AounAli2k06 <aounali2k06@gmail.com>
2024-01-22 03:55:24 -0800

    login layout complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import style from "./App.module.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./services/routes";
-import OTPScreen from "./pages/OTP/OtpScreen";
-import LoginSignupForm from "./pages/loginn/LoginSignupForm";
 
 function App() {
-  
   return (
-      <div className={style["mainDiv"]}>
-        <RouterProvider router={routes} />
-        {/* <OTPScreen/> */}
-      </div>
-  )
+    <div className={style["mainDiv"]}>
+      <RouterProvider router={routes} />
+    </div>
+  );
 }
 
-export default App
+export default App;
